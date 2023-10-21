c92012f3009c7e90f840a6c2fb197057a6197ebc
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-10-21 15:20:08 -0700

    Add additional icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@
 *-------------------------------------------------------------------*/
 
 import React from 'react'
-import { BsGithub, BsTwitter } from 'react-icons/bs'
+import { BsEnvelope, BsGithub, BsLinkedin, BsTwitter, BsFillTelephoneFill } from 'react-icons/bs'
 
 export const Navigation = () => {
   return (
@@ -15,11 +15,20 @@ export const Navigation = () => {
       <nav className="container flex justify-between items-center">
         <div className="font-semibold">React Forms</div>
         <div className="flex items-center gap-4 text-lg">
-          <a href="https://github.com/Yazdun/react-fcc-forms" target="_blank">
+          <a href="https://github.com/" target="_blank">
             <BsGithub />
           </a>
-          <a href="https://twitter.com/Yazdun" target="_blank">
+          <a href="https://twitter.com/" target="_blank">
             <BsTwitter />
+          </a>
+          <a href="https://twitter.com/" target="_blank">
+            <BsLinkedin />
+          </a>
+          <a href="mailto:someone@example.com" target="_blank">
+            <BsEnvelope />
+          </a>
+          <a href="tel:+16042222222" target="_blank">
+            <BsFillTelephoneFill />
           </a>
         </div>
       </nav>
